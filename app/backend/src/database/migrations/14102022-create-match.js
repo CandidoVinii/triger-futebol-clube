4af35984c19ae14105e6ff52b883ff465b3f305a
root <root@SEN002157.sensedia.local>
2022-10-13 11:51:12 -0300

    feat: create migrations and models for matchs  consulting

---
+++ app/backend/src/database/migrations/14102022-create-match.js
@@ -0,0 +1,42 @@
+'use strict';
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('matches', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      homeTeam: {
+        allowNull: false,
+        type: Sequelize.INTEGER,
+        field: 'home_team',
+      },
+      homeTeamGoals: {
+        allowNull: false,
+        type: Sequelize.INTEGER,
+        field: 'home_team_goals',
+      },
+      awayTeam: {
+        allowNull: false,
+        type: Sequelize.INTEGER,
+        field: 'away_team',
+      },
+      awayTeamGoals: {
+        allowNull: false,
+        type: Sequelize.INTEGER,
+        field: 'away_team_goals',
+      },
+      inProgress: {
+        allowNull: false,
+        type: Sequelize.BOOLEAN,
+        field: 'in_progress',
+      },
+    });
+  },
+
+  async down (queryInterface, Sequelize) {
+    await queryInterface.dropTable('matches');
+  }
+};
