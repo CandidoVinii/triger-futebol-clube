8496ec9da5e4b716e8ade47b1de9884249031a0b
root <root@SEN002157.sensedia.local>
2022-10-13 10:09:20 -0300

    feat: create migrations and models for teams consulting

---
+++ app/backend/src/database/migrations/13102022-create-team.js
@@ -0,0 +1,21 @@
+'use strict';
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('teams', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+     teamName: {
+        allowNull: false,
+        type: Sequelize.STRING(50),
+        field: 'team_name',
+      },
+    });
+  },
+  async down (queryInterface, Sequelize) {
+    await queryInterface.dropTable('teams');
+  }
+};
---
+++ app/backend/src/database/models/Team.ts
@@ -0,0 +1,29 @@
+import db from '.';
+import { INTEGER, Model, STRING } from 'sequelize';
+
+class Team extends Model {
+  id!: number;
+  teamName!: string;
+};
+
+Team.init({
+  id: {
+    type: INTEGER,
+    allowNull: false,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+  teamName: {
+    type: STRING,
+    allowNull: false,
+    field: 'team_name',
+  },
+},
+{
+  sequelize: db,
+  modelName: 'teams',
+  timestamps: false,
+}
+);
+
+export default Team;
